01ded8194d6c7dafcf67916dcf8ccd93ceb30df8
AlexandrSemizbayev <alexander.semizbayev@gmail.com>
2025-05-06 20:16:58 -0700

    fix of eslint failure

--- src/hooks/intersectionHook.tsx
+++ src/hooks/intersectionHook.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { createObservableElement } from "@/utils/intersectionObserver";
 import EventBus from "../eventBus";
 import {
@@ -30,7 +30,6 @@ const IntersectionHook = (elementsToObserve: Array<string> = []) => {
         obs.disconnect();
       });
       EventBus.$off(Object.keys(eventsInAir) as string[]);
-      window.removeEventListener("load", loadEvent);
     }
   }, []);
 }
